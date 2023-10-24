c5f988f371a39a5d00202982c5e9e900be5edcad
Alaa Okair <alaaokair330@gmail.com>
2023-10-24 12:49:55 -0700

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a linked list
+ * @h: pointer to the listint_t list to print
+ *
+ * Return: the number of nodes printed
+ */
+
+size_t print_listint(const listint_t *h);
+{
+size_t ls = 0;
+
+while (h)
+{
+printf("%d\n", h->n);
+h = h->next;
+ls++;
+}
+return (ls);
+}
