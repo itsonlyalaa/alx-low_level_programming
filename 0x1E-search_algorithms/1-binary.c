85342b45a0217052a2f69909b0075cb7c92f4ffb
Alaa Okair <alaaokair330@gmail.com>
2024-05-08 18:13:16 -0700

    tasks

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -6,33 +6,43 @@
  * @array: pointer to 1st element of array to serach
  * @size: number of elements
  * @value: value to search for
- * 
+ *
  * Return: 1st index with 'value', or -1 'value' not found
  * or 'array' is NULL
 */
 
 int binary_search(int *array, size_t size, int value)
 {
-    size_t i, lft, ryt, mid;
-    if (array == NULL)
-        return (-1);
-    
-    lft = 0;
-    ryt = size - 1;
+size_t i, lft, ryt, mid;
+if (array == NULL)
+{
+return (-1);
+}
 
-    while (lft <= ryt)
-    {
-        mid = (lft + ryt) / 2;
-        printf("Searching in array: ");
-        for (i = lft; i <= ryt; i++)
-            printf("%i%s", array[i], i == ryt ? "\n" : ",");
-        
-        if (array[mid] < value)
-            lft = mid + 1;
-        else if (array[mid] > value)
-            ryt = mid - 1;
-        else
-            return ((int)mid);
-    }
-    return (-1);
+lft = 0;
+ryt = size - 1;
+
+while (lft <= ryt)
+{
+mid = (lft + ryt) / 2;
+printf("Searching in array: ");
+for (i = lft; i <= ryt; i++)
+{
+printf("%i%s", array[i], i == ryt ? "\n" : ",");
+}
+
+if (array[mid] < value)
+{
+lft = mid + 1;
+}
+else if (array[mid] > value)
+{
+ryt = mid - 1;
+}
+else
+{
+return ((int)mid);
+}
+}
+return (-1);
 }
--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -6,36 +6,47 @@
  * @array: pointer to 1st element of array to serach
  * @size: number of elements
  * @value: value to search for
- * 
+ *
  * Return: 1st index with 'value', or -1 'value' not found
  * or 'array' is NULL
 */
 
 int interpolation_search(int *array, size_t size, int value)
 {
-    size_t high = size - 1;
-    size_t low, pos;
+size_t high = size - 1;
+size_t low, pos;
 
-    if (array == NULL)
-        return (-1);
-    
-    while ((array[high] != array[low]) && (value >= array[low]) && (value <= array[high]))
-    {
-        pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
-        printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
-        if (array[pos] < value)
-            low = pos + 1;
-        else if (value < array[pos])
-            high = pos - 1;
-        else
-            return (pos);
-    }
-    if (value == array[low])
-    {
-        printf("Value checked array[%lu] = [%d]\n", low, array[low]);
-        return (low);
-    }
-    pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
-    printf("Value checked array[%lu] is out of range\n", pos);
-    return (-1);
+if (array == NULL)
+{
+return (-1);
+}
+
+while ((array[high] != array[low]) && (value >= array[low])
+        && (value <= array[high]))
+{
+pos = low + (((double)(high - low) / (array[high] - array[low]))
+             * (value - array[low]));
+printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+if (array[pos] < value)
+{
+low = pos + 1;
+}
+else if (value < array[pos])
+{
+high = pos - 1;
+}
+else
+{
+return (pos);
+}
+}
+if (value == array[low])
+{
+printf("Value checked array[%lu] = [%d]\n", low, array[low]);
+return (low);
+}
+pos = low + (((double)(high - low) / (array[high] - array[low]))
+             * (value - array[low]));
+printf("Value checked array[%lu] is out of range\n", pos);
+return (-1);
 }
